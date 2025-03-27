195d41f0000a9da41a9032d9ef4c5c5ae45c9047
JONATA HENRIQUE DA SILVA CARÁ <26382@unifacef.edu.br>
2025-03-26 22:23:23 -0300

    somando com java

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,10 +1,24 @@
 function calcular(){
-    //recupera o valor da ação social digitado
+    let soma = 0
     //declaração de variável sem tipo
+    //recupera o valor da ação social digitado
     let acaosocial = document.getElementById("acaosocial").value
+    soma = soma + Number(acaosocial)
     //recupe o valor da homenagem digitado
     let homenagem = document.getElementById("homenagem").value
+    soma = soma + Number(homenagem)
+    let leite = document.getElementById("leite").value
+    soma = soma + (2 *leite)
+    let kitavulso = document.getElementById("kitavulso").value
+    soma = soma + (30 * kitavulso)
+    let suplementoavulso = document.getElementById("suplementoavulso").value
+    soma = soma + (15 * suplementoavulso)
+    let arroz = document.getElementById("arroz").value
+    soma = soma + Number(arroz)
+    let feijao = document.getElementById("feijao").value
+    soma = soma + Number(feijao)
+
     //soma os valores
-    let soma = Number(acaosocial) + Number(homenagem)
-    alert(soma)
+    //devolve o resultado para o HTML
+    document.getElementById("soma").innerHTML = soma.toFixed(2)
 }
