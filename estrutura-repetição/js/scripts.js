36ea6704cfa5dc9fa786dc55fa5d88cfb228072d
JONATA HENRIQUE DA SILVA CARÁ <26382@unifacef.edu.br>
2025-05-07 22:01:22 -0300

    exe0

--- estrutura-repetição/js/scripts.js
+++ estrutura-repetição/js/scripts.js
@@ -10,3 +10,26 @@ function exe0(){
     let media = soma / 6
     alert(media)
 }
+
+function pesquisaSatisfacao(){
+    let conta = 1 // numero de participantes
+    let nota // será informado pelo usuário
+    let soma = 0 // soma as notas para a média
+    let satisfeitos = 0 // inicialmente, nenhum satisfeito 
+    let insatisfeito = 0 // inicialmente, nenhum insatisfeito
+    while(conta <= 10){
+        nota = Number(prompt(`Informe a nota (0 a 10) do participante ${conta}`))
+        if (nota >= 8 && nota <= 10){
+            satisfeitos++ // satisfeitos = satisfeitos + 1
+        } else if (nota >= 0 && nota < 5){
+            insatisfeito++ // insatisfeitos = insatisfeitos + 1
+        } else if (nota < 0 || nota > 10){ // || é OU
+            alert(`Nota inválida!`)
+            continue // volta para o início do while
+        }
+        soma += nota // soma as notas informadas
+        conta++ // conta = conta + 1
+    }
+    let media = soma / 10 // calcula a média das notas
+    alert(`Satisfeitos: ${satisfeitos}\nInsatisfeitos: ${insatisfeito}\nMédia: ${media.toFixed(1)}`)
+}
