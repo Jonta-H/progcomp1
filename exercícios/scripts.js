0dfe22a180acc7f6264740572150c3385253a239
Jonata Henrique <jonatah.1512@gmail.com>
2025-05-18 21:40:33 -0300

    exe4, exe5, exe6

--- exercícios/scripts.js
+++ exercícios/scripts.js
@@ -150,3 +150,60 @@ function exercicio3(){
     )
 }
 
+function exe4(){
+    let i, cont, result
+    let tabuada = ""
+
+    i = Number(prompt("Informe um número para receber sua tabuada"))
+
+    for(cont = 0; cont<=10; cont++){
+        result = cont * i
+        tabuada+= `${i} x ${cont} = ${result}\n`
+    }
+
+    alert(tabuada)
+}
+
+function exe5(){
+    let n, cont, result
+
+    for(cont = 1; cont<=10; cont++){
+      let tabuada = ""  
+        for(n = 0; n <= 10; n++){
+            result = cont * n
+            tabuada+= `${cont} x ${n} = ${result}\n`
+            
+        }
+       alert(tabuada) 
+    }
+}
+
+function exe6(){
+    let V = 0, P = 0, valor, cont, opcao, valorTotal
+
+    for(cont = 1; cont <= 15; cont++){
+        opcao = prompt("V = à vista\nP = a prazo")
+            if(opcao != "V" && opcao != "v" && opcao != "P" && opcao != "p"){
+                alert("Informe uma opção válida")
+                cont--
+                continue
+            }
+        valor = Number(prompt("Informe o valor da compra:"))
+            if(valor <= 0){
+                alert("Informe um valor válido")
+                cont--
+                continue
+            }
+        if(opcao === "V" || opcao === "v"){
+            V+= valor
+        } else{
+            P+= valor
+        }
+    }
+    valorTotal = V + P
+    alert(`Valor total das compras à vista: R$${V}\n`
+        + `Valor total das compras a prazo: R$${P}\n`
+        + `Valor total das compras efetuadas: R$${valorTotal}\n`
+        + `Valor da primeira prestação a prazo: R$${(P/3).toFixed(2)}`
+    )
+}
